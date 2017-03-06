dd4563db038e0c5a7c09d34bef3d03b6f30c0b1d
James Hwang <91juhwang@gmail.com>
2017-03-06 18:22:25 -0500

    Add unsuccessful test case for ads_controoler_spec

--- app/controllers/api/v1/ads_controller.rb
+++ app/controllers/api/v1/ads_controller.rb
@@ -2,8 +2,10 @@ module Api
   module V1
     class AdsController < ApplicationController
       def create
-        ad = Ad.create(ad_params)      
-        render json: { message: 'Ad created successfully', id: ad.id}
+        ad = Ad.create!(ad_params)      
+        render json: { message: 'Ad created successfully', id: ad.id }
+      rescue ActiveRecord::RecordInvalid => invalid
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity 
       end
 
       private
--- spec/controllers/ads_controller_spec.rb
+++ spec/controllers/ads_controller_spec.rb
@@ -2,22 +2,42 @@ require 'rails_helper'
  
 RSpec.describe Api::V1::AdsController, type: :controller do
   describe 'POST create' do
+    let(:ad) { FactoryGirl.build(:ad, id: 1) }
     let(:params) { FactoryGirl.attributes_for(:ad).merge(user_id: '1') }
+    let(:permitted_params) { ActionController::Parameters.new(params) }
     let(:execute_request) { post :create, params: params }
 
-    context 'when parameters are valid' do 
-      before do 
-        ActionController::Parameters.permit_all_parameters = true
-        permitted_parameters = ActionController::Parameters.new(params)
-        expect(Ad).to receive(:create).with(permitted_parameters).and_return(FactoryGirl.build(:ad, id: 1))
+    before(:all) do 
+      ActionController::Parameters.permit_all_parameters = true
+    end
+
+    context 'when valid parameters are passed' do
+      before do
+        expect(Ad).to receive(:create).with(permitted_params).and_return(FactoryGirl.build(:ad, id: 1))
         execute_request
       end
-      it 'should return a successful response' do 
+
+      it 'should return a successful response' do
         expect(response).to have_http_status(:success)
       end
 
       it 'should return success message with an ad_id' do
         expect(JSON.parse(response.body)).to eq(JSON.parse('{"message": "Ad created successfully", "id": 1 }'))
+      end
+    end
+
+    context 'when invalid parameters are passed' do
+      before do
+        ad.errors.add(:title, "can't be blank")
+        expect(Ad).to receive(:create!).with(permitted_params).and_raise(ActiveRecord::RecordInvalid.new(ad))
+        execute_request
+      end
+      it 'should return model validation errors' do
+        expect(JSON.parse(response.body)).to eq(JSON.parse('{"errors": {"title": ["can\'t be blank"]}}'))
+      end
+
+      it 'should return unprocessable entity response' do
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
